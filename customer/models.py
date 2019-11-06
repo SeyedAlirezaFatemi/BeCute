9afafee0f28fc3221b3815583eccbefedd64288d
abdijavad110 <abdijavad110@gmail.com>
2019-11-06 17:23:07 +0330

    fix:dev: fix fields

--- customer/models.py
+++ customer/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 class Service(models.Model):
-    name = models.CharField(max_length="50")
+    name = models.CharField(max_length=50)
     duration = models.DurationField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
 
 class BarberShop(models.Model):
+    name = models.CharField(max_length=50)
     # barber = models.ForeignKey(USER, on_delete=models.CASCADE) TODO link to barber
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
