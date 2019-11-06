aff5b4c611f6eddc909aeb84609ace71ba06290c
abdijavad110 <abdijavad110@gmail.com>
2019-11-06 17:03:40 +0330

    new:dev: add some models barbershop, service, reservation

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length="50")
+    duration = models.DurationField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class BarberShop(models.Model):
+    # barber = models.ForeignKey(USER, on_delete=models.CASCADE) TODO link to barber
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
