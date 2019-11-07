eae44ceabfc7c8bbf28ab0420205c04a3ccbfc7a
halikhani <h.alikhani.k@gmail.com>
2019-11-07 14:05:59 +0330

    urls for barber and customer setted

--- customer/views.py
+++ customer/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def main(request):
     return render(request, 'customer/index.html', context={})
+
+
+def reserve(request):
+    return HttpResponse(" this is search page of costumer")
+
+
+def search(request):
+    return HttpResponse(" this is reserve page of costumer")
+
+
+def profile(request):
+    return HttpResponse(" this is profile page of costumer")
