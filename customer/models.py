cee48a2f289b747433055c5eb837b3bd228e42b1
Mreza Gh <mrezagh.legend@gmail.com>
2019-11-07 19:36:06 +0330

    Signup, Login, Logout done
    you can go to: 
    /accounts/signup for signup
    /accounts/login for login
    /accounts/logout for logout

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, User
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    type = models.CharField(max_length=100, choices=(('client', 'client'), ('barber', 'barber')))
+
+
