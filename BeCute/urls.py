0946dabec12f0987db00945575b8520ece04cf92
Alireza Fatemi <alirezafa@rocketmail.com>
2020-02-05 11:55:16 +0330

    :lipstick: Added LandingPage.

--- BeCute/urls.py
+++ BeCute/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from BeCute.views import logout_view
+from BeCute.views import logout_view, index_view
 
 urlpatterns = [
+    path('', index_view, name='index'),
     path("admin/", admin.site.urls),
     path("customers/", include("customer.urls")),
     path("barbers/", include("barber.urls")),
--- BeCute/views.py
+++ BeCute/views.py
@@ -2,6 +2,10 @@ from django.contrib.auth import logout
 from django.shortcuts import render
 
 
+def index_view(request):
+    return render(request, "index.html")
+
+
 def logout_view(request):
     if request.user.is_authenticated:
         logout(request)
