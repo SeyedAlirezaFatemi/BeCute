e05c42f94f7b44867ee6d30e71db9479e5e1094f
Alireza Fatemi <alirezafa@rocketmail.com>
2020-02-05 12:26:13 +0330

    :lipstick: New Login Page + :bug: Fixing a bug.

--- account/views.py
+++ account/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.views import LoginView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 
 from BeCute.misc import get_login_redirect_url, get_signup_redirect_url
@@ -13,6 +13,15 @@ def landing(request):
 class Signup(generic.CreateView):
     template_name = "registration/signup.html"
     form_class = SignupForm
+
+    def get(self, request, *args, **kwargs):
+        try:
+            if request.user:
+                if request.user.type == 'barber':
+                    return redirect("barber-profile")
+                return redirect("customer-profile")
+        except:
+            return super().get(self, request, *args, **kwargs)
 
     def get_success_url(self):
         return get_signup_redirect_url()
